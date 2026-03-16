a0a287b49fb7f9a91a0f7390150e6fe41c64b905
bhosalesb07 <bhosalesb07@gmail.com>
2026-03-16 15:22:12 +0530

    Initial Commit

---
+++ SettingScreen_SwitfUI/SettingScreen_SwitfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingScreen_SwitfUIApp.swift
+//  SettingScreen_SwitfUI
+//
+//  Created by Swapnil B on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SettingScreen_SwitfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SettingScreen_SwitfUITests/SettingScreen_SwitfUITests.swift
@@ -0,0 +1,17 @@
+//
+//  SettingScreen_SwitfUITests.swift
+//  SettingScreen_SwitfUITests
+//
+//  Created by Swapnil B on 16/03/26.
+//
+
+import Testing
+@testable import SettingScreen_SwitfUI
+
+struct SettingScreen_SwitfUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
